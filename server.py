3039d17d7d3248fd5ccaa452f9dfc4b684916ecc
Sriharsha Kotcharlakot <k.venkatsriharsha@gmail.com>
2021-01-10 22:53:23 +0530

    Added a Glitch Python Flask project. :zap:

---
+++ server.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from flask import render_template
+from flask import request
+import model
+
+
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+  return render_template("index.html")
+  
+if __name__ == "__main__":
+  app.run()
